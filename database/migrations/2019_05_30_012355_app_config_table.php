92d122584c6590dbdd09f9a051fb15b879d4be91
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2019-05-30 08:17:21 +0100

    add app config table

---
+++ database/migrations/2019_05_30_012355_app_config_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AppConfigTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app_configs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('screen_id')->nullable();
+            $table->string('key');
+            $table->text('value')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app_configs');
+    }
+}
