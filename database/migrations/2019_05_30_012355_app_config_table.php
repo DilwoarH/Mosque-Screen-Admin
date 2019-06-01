4bb9d3ad0c8d5a43bd0825177e972d181ec1f22a
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2019-06-01 02:01:33 +0100

    changes app config table structure

--- database/migrations/2019_05_30_012355_app_config_table.php
+++ database/migrations/2019_05_30_012355_app_config_table.php
@@ -16,8 +16,20 @@ class AppConfigTable extends Migration
         Schema::create('app_configs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('screen_id');
-            $table->string('key');
-            $table->text('value')->nullable();
+            $table->string('logo_url')->nullable();
+            $table->string('primary_colour')->default('#0f715d');
+            $table->string('secondary_colour')->default('#0c5a4b');
+            $table->string('primary_text_colour')->default('#fff');
+            $table->string('secondary_text_colour')->default('#e4e4e4');
+            $table->string('prayer_time_highlight_colour')->default('#ff3995');
+            $table->string('clock_background_colour')->default('#fff');
+            $table->string('time_format')->default('h:mm A');
+            $table->string('slider_mode')->default('slider');
+            $table->integer('slider_timeout')->default(8000);
+            $table->text('text_donation_message')->nullable();
+            $table->text('online_donation_message')->nullable();
+            $table->text('mobile_silence_message')->nullable();
+            $table->text('single_view_message')->nullable();
             $table->timestamps();
         });
     }
